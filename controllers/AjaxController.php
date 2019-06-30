58c2a5db1d0af8b6fbe46c01b5d9c7b97b8c096a
ruslan <rendzhi@mail.ru>
2019-06-30 12:07:59 +0700

    диагностика доделать

--- controllers/AjaxController.php
+++ controllers/AjaxController.php
@@ -107,8 +107,8 @@ class AjaxController extends Controller
                     $_problems = $repair->getCurrentDeviceProblems(false,$_post['problems']);
                     if(!empty($_repair) && !empty($_devices) && !empty($_problems)) {
                         $html = 'Устройства: ' . $_repair->title . ' ';
-                        $html .= ' Модель' . $_devices->title . ' ';
-                        $html .= ' Проблема' . $_problems->title . ' ';
+                        $html .= ' Модель: ' . $_devices->title . ' ';
+                        $html .= ' Проблема: ' . $_problems->title . ' ';
                         $call->comments = $html;
                     }
                 }
