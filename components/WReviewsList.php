3a6f9b5dd46a434b3e847242baff6cffcdf8c870
ruslan <rendzhi@mail.ru>
2019-08-12 16:58:16 +0700

    диагностика доделать

--- components/WReviewsList.php
+++ components/WReviewsList.php
@@ -18,25 +18,29 @@ class WReviewsList extends Widget {
     }
     public function run(){
 
-        if(!empty($this->reviews)) {
+        if(!empty($this->review)) {
             ?>
             <tr>
                 <td class="image center" data-label="">
-                    <img src="<?=$this->reviews->img?>" class="size-2">
+                    <img src="<?=$this->review->img?>" class="size-2">
                     <div class="name">
-                        <?=$this->reviews->name?>
-                        <div class="rating" style="position: relative; height: 18px"><div class="rating-icon rating-<?=$this->reviews->rating?>"></div></div>
-                        <div class="small"><?=!empty($this->reviews->date_created) ? date('d.m.Y',strtotime($this->reviews->date_created)).' г.' : date('d.m.Y',strtotime($this->reviews->date))?> г.</div>
+                        <?=$this->review->name?>
+                        <div class="rating" style="position: relative; height: 18px"><div class="rating-icon rating-<?=$this->review->rating?>"></div></div>
+                        <div class="small"><?=!empty($this->review->date_created) ? date('d.m.Y',strtotime($this->review->date_created)).' г.' : date('d.m.Y',strtotime($this->review->date))?> г.</div>
                     </div>
                 </td>
                 <td class="text-center center">
-                    <div class="description"><?=$this->reviews->text?></div>
+                    <div class="description"><?=$this->review->text?></div>
                 </td>
             </tr>
             <?php
         }else{
             ?>
-
+            <tr>
+                <td colspan="2">
+                   <div class="text-center">Нет отзывы</div>
+                </td>
+            </tr>
             <?php
         }
     }
