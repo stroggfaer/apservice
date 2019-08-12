b95616141b2a547a74b228018f7de94e67a1b467
ruslan <rendzhi@mail.ru>
2019-08-12 17:13:50 +0700

    диагностика доделать

--- modules/cms/models/modules/ParserReviewsGis.php
+++ modules/cms/models/modules/ParserReviewsGis.php
@@ -37,8 +37,9 @@ class ParserReviewsGis extends Model
 
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
         $result = curl_exec($ch);
         $result = json_decode($result, true);
         curl_close($ch);
